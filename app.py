c09682d39c39d1c300f65ab45a0d60e401f19cae
ShreyasPawar553 <86672016+ShreyasPawar553@users.noreply.github.com>
2024-11-19 08:35:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,8 @@ def disease_predict():
         return jsonify({"error": f"Error processing image: {str(e)}"}), 500
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
-
+    port = int(os.environ.get('PORT', 5000))  # Get port from environment or default to 5000
+    app.run(host='0.0.0.0', port=port)
